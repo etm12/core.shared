5d430f6f8d1ce28f7717cec673b51985cbd591f0
Stefan Rimaila <stefan@rimaila.fi>
2019-02-20 22:05:17 +0200

    feat(typedef): Add typedefs

---
+++ lib/index.d.ts
@@ -0,0 +1,57 @@
+import { Stream, Property } from 'kefir';
+
+declare let S: S.Static;
+
+declare namespace S {
+  type Pair<T0, T1> = [T0, T1];
+  type Position = Pair<number, number>;
+  type PositionIx = { start: number, end: number };
+
+  type Ary0Fn<R> = () => R;
+  type Ary1Fn<T1, R> = (t1: T1) => R;
+  type Ary2Fn<T1, T2, R> = (t1: T1, t2: T2) => R;
+  type Ary3Fn<T1, T2, T3, R> = (t1: T1, t2: T2, t3: T3) => R;
+  type Ary4Fn<T1, T2, T3, T4, R> = (t1: T1, t2: T2, t3: T3, t4: T4) => R;
+
+  interface CurriedGuardAry2<T1, T2, R extends T2> {
+    (t1: T1): (t2: T2) => t2 is R;
+    (t1: T1, t2: T2): t2 is R;
+  }
+
+  interface CurriedGuardAry3<T1, T2, T3, R extends T3> {
+    (t1: T1): CurriedGuardAry2<T2, T3, R>;
+    (t1: T1, t2: T2): (t3: T3) => t3 is R;
+    (t1: T1, t2: T2, t3: T3): t3 is R;
+  }
+
+  interface CurriedAry2<T1, T2, R> {
+    (t1: T1): (t2: T2) => R;
+    (t1: T1, t2: T2): R;
+  }
+
+  interface CurriedAry3<T1, T2, T3, R> {
+    (t1: T1): CurriedAry2<T2, R>;
+    (t1: T1, t2: T2): (t3: T3) => R;
+    (t1: T1, t2: T2, t3: T3): R;
+  }
+
+  interface Static {
+    flip<T1, T2, R>(fn: Ary2Fn<T1, T2, R>, t2: T2, t1: T1): R;
+    flip<T1, T2, R>(fn: Ary2Fn<T1, T2, R>, t2: T2): (t1: T1) => R;
+    flip<T1, T2, R>(fn: Ary2Fn<T1, T2, R>): (t2: T2) => (t1: T1) => R;
+
+    invoke0<R>(m: string, f: Function): R;
+
+    invoke1<T1, R>(m: string, x: T1, f: Ary1Fn<T1, R>): R;
+    invoke1<T1, R>(m: string, x: T1): (f: Ary1Fn<T1, R>) => R;
+    invoke1<T1, R>(m: string): CurriedAry2<T2, R>;
+
+    invoke2<T1, T2, R>(m: string, t1: T1, t2: T2, f: Ary2Fn<T1, T2, R>): R;
+    invoke2<T1, T2, R>(m: string, t1: T1, t2: T2): (f: Ary2Fn<T1, T2, R>) => R;
+    invoke2<T1, T2, R>(m: string, t1: T1): CurriedAry2<T2, R>;
+    invoke2<T1, T2, R>(m: string): CurriedAry3<T1, T2, R>;
+  }
+}
+
+export = S;
+export as namespace S;
